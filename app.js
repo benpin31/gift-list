89b9a8a38db92be64e4c2ed895ef43442a70b04e
benpin31 <benpin31@gmail.com>
2021-03-09 10:07:41 +0100

    modify list by lists

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/list', listRouter);
+app.use('/lists', listRouter);
 
 
 // catch 404 and forward to error handler
